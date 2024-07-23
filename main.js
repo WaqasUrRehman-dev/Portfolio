1993ef150eabc9865c5cb860df40e5419dcb1bfd
Waqas Ur Rehman <rehmanwaqas466@gmail.com>
2024-07-24 00:40:17 +0500

    completed for only dekstop devices

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+  strings: ["MERN Stack Developer", "Backend Developer", "Frontend Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
